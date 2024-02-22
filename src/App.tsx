9daa781fe30ba3aa7d6e16d68824bf9cc86d8ae5
electrixky <tanyatkachuk2401@gmail.com>
2024-02-22 20:55:40 +0200

    set start value

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,34 @@ import {Counter} from "./components/Counter";
 
 function App() {
     let [counter,setCounter] = useState(0)
+    let [maxValue, setMaxValue] = useState(0)
+    let [startValue, setStartValue] = useState(0)
 
-    const increment = () => {
-        console.log("counter")
+    const incrementHandler = () => {
         setCounter(++counter)
     }
 
-    const reset = () => {
-        console.log("reset")
+    const resetHandler = () => {
         setCounter(0)
+    }
+
+    const setHandler = () => {
+        setCounter(startValue)
+    }
+
+    const updateMaxValue = (newValue: number) => {
+        setMaxValue(newValue)
+    }
+
+    const updateStartValue = (newValue: number) => {
+        setStartValue(newValue)
     }
 
 
     return (
     <div className="App">
-      <Counter isSettingsCounter/>
-      <Counter value={counter} onIncrement={increment} onReset={reset}/>
+      <Counter isSettingsCounter updateMaxValue={updateMaxValue} updateStartValue={updateStartValue} onSet={setHandler}/>
+      <Counter value={counter} onIncrement={incrementHandler} onReset={resetHandler} maxValue={maxValue} startValue={startValue}/>
     </div>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {Button} from "./Button";
 
 type CounterPropsType = {
     value?: number
     onIncrement?: () => void
     onReset?: () => void
+    onSet?: () => void
     isSettingsCounter?: boolean
+    maxValue?: number
+    startValue?: number
+    updateMaxValue?: (newValue: number) => void
+    updateStartValue?: (newValue: number) => void
 }
 
-export const Counter = ({value, onIncrement, onReset, isSettingsCounter}: CounterPropsType) => {
+export const Counter = ({
+                            value,
+                            onIncrement,
+                            onReset,
+                            isSettingsCounter,
+                            maxValue,
+                            startValue,
+                            updateMaxValue,
+                            updateStartValue,
+                            onSet
+                        }: CounterPropsType) => {
 
+    const maxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const newMaxValue = parseInt(e.currentTarget.value, 10)
+        if (!isNaN(newMaxValue) && updateMaxValue) {
+            updateMaxValue(newMaxValue)
+        }
+    }
+
+    const startValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const newStartValue = parseInt(e.currentTarget.value, 10)
+        if (!isNaN(newStartValue) && updateStartValue) {
+            updateStartValue(newStartValue)
+        }
+    }
 
     return (
         <div className={"counter"}>
@@ -17,12 +45,18 @@ export const Counter = ({value, onIncrement, onReset, isSettingsCounter}: Counte
                 {isSettingsCounter ?
                     <>
                         <div>
-                            <span>max value:</span>
-                            <input/>
+                            <label htmlFor={"max"} className={"input-title"}>max value:
+                                <input id={"max"} type="number" className={"input"}
+                                       onChange={maxValueHandler}
+                                />
+                            </label>
                         </div>
                         <div>
-                            <span>start value:</span>
-                            <input/>
+                            <label htmlFor={"start"} className={"input-title"}>start value:
+                                <input id={"start"} type="number" className={"input"}
+                                       onChange={startValueHandler}
+                                />
+                            </label>
                         </div>
                     </>
                     : <span className={"counter-number"}>{value}</span>
@@ -32,9 +66,8 @@ export const Counter = ({value, onIncrement, onReset, isSettingsCounter}: Counte
                 {!isSettingsCounter ? <>
                         <Button callBack={onIncrement} title={"inc"}/>
                         <Button callBack={onReset} title={"reset"}/></>
-                    : <Button title={"set"}/>
+                    : <Button callBack={onSet} title={"set"}/>
                 }
-
             </div>
         </div>
     );
